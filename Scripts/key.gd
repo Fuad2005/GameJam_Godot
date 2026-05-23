509a7434649883d221ae1c21dec8b21eb6fa5f64
NiyazliKhanali <niyazlikhanali@gmail.com>
2026-05-22 23:03:58 -0700

    key-door logic done

---
+++ Scripts/key.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var key_name: String = "GoldKey"
+
+func _ready():
+	add_to_group("Keys")
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "Player":
+		Global.key = true   # set the global key boolean
+		queue_free()        # remove the key from the scene
+		print("Picked up key!")
